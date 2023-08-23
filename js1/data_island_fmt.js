b48d98912ed21ecff20d1458c3980f34e7f123c3
James Crook <james.k.crook@gmail.com>
2023-08-23 21:20:50 +0100

    Draw graphs off DataIsland

--- js1/data_island_fmt.js
+++ js1/data_island_fmt.js
@@ -1,19 +1,57 @@
 
 
 // A null format that does not emit content.
-function DatIsland_Fmt(){
+function DataIsland_Fmt(){
   return this;
 }
 
-DatIsland_Fmt.prototype ={
+DataIsland_Fmt.prototype ={
   name : "DataIsland",
   debug(A,url,text){
     alert( url );
   },
+  delayed_quick_plot( canv ){
+    var ctx = canv.getContext('2d');
+    var data = this.data;
+    var rowHeight =20;
+    var graphWidth = 500;
+    var nRows = data.length -1;
+    ctx.font = "14px Arial";
+    ctx.fillStyle = "#000070";
+    ctx.textAlign = "left";
+    graphWidth = 100;
+    var max = [];
+    for( var col = 2;col<=5;col++){
+      var m=0;
+      for(var i=1;i<=nRows;i++){
+        var t = data[i][col];
+        if(t>m)
+          m=t;
+        max[col]= m;
+      }
+    }
+    for( var i=1;i<=nRows;i++){
+      text = data[i][1];
+      ctx.fillText(text, 20, i*rowHeight + 10);
+      for( var col=2;col<=5;col++){
+        var x = 100 + (col-2) * (graphWidth+5);
+        var x1 = x+ (graphWidth*data[i][col])/max[col];
+        var y = i*rowHeight;
+        var y1 = y+ rowHeight - 5;
+        ctx.fillRect( x,y,x1-x,y1-y);
+      }
+    }
+  },
   quick_plot( spec ){
     var data = spec.data;
-    return "Fields are: " + data[0];
-
+    this.data = data;
+    var result = "";
+    result += "Fields are: " + data[0];
+    var rowHeight =20;
+    var graphWidth = 500;
+    var nRows = data.length -1;
+    result+=`<canvas width=${graphWidth} height=${nRows*rowHeight+30}>Canvas not supported</canvas><img src onerror='DataIsland_Fmt.delayed_quick_plot(this.previousSibling)'><br>Bar`;
+    return result;
   },
   htmlOf( text ){
     var spec = JSON.parse(text);
@@ -21,5 +59,5 @@ DatIsland_Fmt.prototype ={
   }
 }
 
-DatIsland_Fmt = new DatIsland_Fmt();
-Registrar.register( DatIsland_Fmt );
+DataIsland_Fmt = new DataIsland_Fmt();
+Registrar.register( DataIsland_Fmt );
